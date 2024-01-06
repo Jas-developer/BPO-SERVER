6434eed258992503844c9c14d036961a71477d8f
Jason <devop.jas@gmail.com>
2024-01-06 09:56:52 +0800

    Created jobs functionality

--- src/controller/admin.js
+++ src/controller/admin.js
@@ -1,6 +1,7 @@
 import Admin from "../model/admin.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import Jobs from "../model/jobs.js";
 
 /*
 @desc SIGN UP 
@@ -54,21 +55,76 @@ const Login = async (req, res) => {
       expiresIn: "1h",
     });
 
-    if (isMatch) {
-      res.status(200).json({
-        name: admin.name,
-        email: admin.email,
-        token: token,
+    if (!isMatch) {
+      res.status(403).json({
+        message: "Invalid credintials",
       });
     }
+    res.status(200).json({
+      _id: admin._id,
+      name: admin.name,
+      email: admin.email,
+      token: token,
+    });
   } catch (error) {
     console.log(error);
   }
 };
 
-/*
+/* 
 desc POST
 desc POST
+will create a job
 */
 
-export { SignUp, Login };
+const createJob = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, salary, requirements, description, image } = req.body;
+
+    // Getting the admin
+    const admin = await Admin.findById(id);
+    if (!admin) {
+      return res.status(404).json({
+        message: "Admin not found",
+      });
+    }
+
+    if (!title || !salary || !requirements || !image) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    // Modify the filename in the frontend to make them the same with the title to access the image path
+    const imagePath = `public/uploads/${image}`; // Correcting the imagePath
+
+    // Create the job object
+    const newJob = {
+      title: title,
+      image: image,
+      salary: salary,
+      requirements: requirements,
+      description: description,
+      imagePath: imagePath,
+    };
+
+    // Push the new job to the admin's jobs array
+    const createdJob = await Jobs.create(newJob);
+
+    if (!createdJob) {
+      return res.status(400).json({
+        message: "Unable to create new jobs",
+      });
+    }
+
+    admin.jobs.push(createdJob._id);
+    await admin.save();
+
+    res.status(201).json({ message: "Job created successfully", job: newJob });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export { SignUp, Login, createJob };
